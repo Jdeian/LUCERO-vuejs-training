19e2e8da180f2bcc10da5fbd765e6a35e421f1d6
Jade Ian Lucero <jade.ian.lucero@ibm.com>
2026-07-01 10:14:53 +0800

    feat: install router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TaskCounter from '../views/day1_taskCounter.vue'
-// import TaskCardDay2 from '../views/TaskCard_day2.vue'
 import TaskListViewDay2 from '../views/TaskListView_day2.vue'
 
 const router = createRouter({
@@ -11,11 +10,6 @@ const router = createRouter({
       name: 'day1-counter',
       component: TaskCounter
     },
-    // {
-    //   path: '/day2-card',
-    //   name: 'day2-card',
-    //   component: TaskCardDay2
-    // },
     {
       path: '/day2-list',
       name: 'day2-list',
